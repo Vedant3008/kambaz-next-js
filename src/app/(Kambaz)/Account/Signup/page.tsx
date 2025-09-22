d8cbd6024fea2d8a5e484b1a5fa386a8d7023976
Vedant3008 <shahh7107@gmail.com>
2025-09-22 00:53:10 -0400

    Assignment1

--- src/app/(Kambaz)/Account/Signup/page.tsx
+++ src/app/(Kambaz)/Account/Signup/page.tsx
@@ -4,9 +4,9 @@ export default function Signup() {
   return (
     <div id="wd-signup-screen">
       <h3>Sign up</h3>
-      <input placeholder="username" className="wd-username" /><br/>
-      <input placeholder="password" type="password" className="wd-password" /><br/>
-      <input placeholder="verify password" type="password" className="wd-password-verify" /><br/>
+      <input placeholder="username" className="wd-username" defaultValue="Vedant_Shah" /><br/>
+      <input placeholder="password" type="password" className="wd-password" defaultValue="password123" /><br/>
+      <input placeholder="verify password" type="password" className="wd-password-verify" defaultValue="password123" /><br/>
       <Link href="/Account/Profile"> Sign up </Link><br />
       <Link href="/Account/Signin"> Sign in </Link>
     </div>
---
+++ src/app/(Kambaz)/Courses/[cid]/Grades/page.tsx
@@ -0,0 +1,8 @@
+export default function Grades() {
+  return (
+    <div id="wd-grades">
+      <h3>Grades</h3>
+      <p>Grades placeholder</p>
+    </div>
+  );
+}
